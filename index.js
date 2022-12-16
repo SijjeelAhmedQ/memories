1561dc9bed63524fe60bc1ac8583c292cd47a986
SijjeelAhmedQ <luckyqureshi707@gmail.com>
2022-12-16 18:49:43 +0500

    beshak

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ dotenv.config()
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use('/posts', postRoutes);
 app.get('/', (req, res)=>{
     res.send('Hello to Memories API')
 })
+app.use('/posts', postRoutes);
 
 // const CONNECTION_URL = "mongodb+srv://SijjeelAhmed:O7nkS9qMGTUpmlzk@cluster0.crg6dc8.mongodb.net/?retryWrites=true&w=majority"
 const PORT = 3000;
@@ -22,6 +22,6 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(()=> app.listen(PORT, ()=> console.log(`Server running on PORT: ${PORT}`)))
 .catch((error)=> console.log(error.message));
-app.listen(PORT, ()=> console.log(`Server running on PORT: ${PORT}`))
+// app.listen(PORT, ()=> console.log(`Server running on PORT: ${PORT}`))
 // mongoose.set('useFindAndModify', false)
 // mongoose.set('strictQuery', true);
