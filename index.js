c3cb8b6a536df17ec323db5ad37958628b62da35
Sijjeel Ahmed Qureshi <sijjelahmedqureshiofficial@gmail.com>
2022-12-16 17:30:56 +0500

    second commit

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use('/posts', postRoutes);
+app.get('/', (req, res)=>{
+    res.send('Hello to Memories API')
+})
 
 // const CONNECTION_URL = "mongodb+srv://SijjeelAhmed:O7nkS9qMGTUpmlzk@cluster0.crg6dc8.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
